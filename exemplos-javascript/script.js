5d185ed9c6966841c6177fc482d4200921dbaf40
Lara Kanashiro <162447628+LaraKanashiro@users.noreply.github.com>
2024-03-29 07:08:52 +0000

    diper e babySize

--- exemplos-javascript/script.js
+++ exemplos-javascript/script.js
@@ -1,1 +1,31 @@
-console.log('Olá mundo!');
+let diaper
+let babySize = 67;
+
+if (typeof babySize != 'number') {
+    diaper = 'Valor invalido!'
+}
+if (babySize <= 4) {
+    diaper = 'RN';
+}
+else if (babySize > 4 && babySize <= 6) {
+    diaper = 'RN+';
+}
+else if (babySize > 6 && babySize <= 8) {
+    diaper = 'P';
+}
+else if (babySize > 8 && babySize <= 10) {
+    diaper = 'M';
+}
+else if (babySize > 10 && babySize <= 13) {
+    diaper = 'G';
+}
+else if (babySize > 13 && babySize <= 15) {
+    diaper = 'XG';
+}
+else if (babySize > 15) {
+     diaper = 'XXG'
+}
+
+
+console.log('O tamaho da fralda é: ' + diaper);
+
