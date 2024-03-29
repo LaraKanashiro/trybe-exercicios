18c449dc1f126640a27668f8c8ba4ec54bfc7b4b
Lara Kanashiro <162447628+LaraKanashiro@users.noreply.github.com>
2024-03-29 08:53:18 +0000

    CSGO

--- exemplos-javascript/script.js
+++ exemplos-javascript/script.js
@@ -1,55 +1,49 @@
-//CSGO - Cáculo de dinehrio 
-
 const round = 2; //Entre 1 e 16
 let money = 1200;
-const killWeapon = 'Knife'; //'Knife', 'AWP', 'Desert Eagle', 'Motolov', 'MP7'
+const killWeapon = 'Knife'; //'Knife', 'AWP', 'Desert Eagle', 'Molotov', 'MP7'. 
 const win = true; //false
-const howwin = 'Bomb explode'; //'Eliminado', 'Tempo esgotado', 'Bomb has been planted', 'Bomb has been defused', 'Bomb explode'.
+const howWin = 'Bomb explode'; //'Eliminando', 'Tempo esgotado', 'Bomb has been planted', 'Bomb has been defused', 'Bomb explode'.
 const defuseBomb = true; //false
 const bombDetonate = false; //true
 
 if (round === 1 || round === 16) {
-    money = 800;
-}
-else if (win === (true || howWin === 'Eliminado' || howWin === 'Tempo esgotado')) {
-    money += 3250;
-}
-else if (win === true || howWin === 'Bomb has been planted') {
-    money += 3550;
-}
-else if (win === true && (howWin === 'Bomb has been defused' || howWin === 'Bomb explode')) {
-    money += 3500;
+  money = 800;
+} else if (win === true && (howWin === 'Eliminando' || howWin === 'Tempo esgotado')) {
+  money += 3250;
+} else if (win === true && howWin === 'Bomb has been planted') {
+  money += 3550;
+} else if (win === true && (howWin === 'Bomb has been defused' || howWin === 'Bomb explode')) {
+  money += 3500;
 }
 
-if (round !== 1 || round !== 16){
-    switch (killWeapon) {
-      case 'Knife':
-    }}
-    money += 1500;
-    break;
-     case'AWP';
-      money +=100;
+if (round !== 1 && round !== 16) {
+  switch (killWeapon) {
+    case 'Knife':
+      money += 1500;
       break;
-       case'Desert Eagle';
+    case 'AWP':
+      money += 100;
+      break;
+    case 'Desert Eagle':
       money += 300;
       break;
-       case'Molotov';
+    case 'Molotov':
       money += 300;
       break;
-       case'MP7';
+    case 'MP7':
       money += 600;
       break;
-      
-        console.log('Arma não encontrada');
-        break;
-  
+    default:
+      console.log('Arma não encontrada.');
+      break;
+  }
+}
 
 if (money > 16000) {
   money = 16000;
 }
 
 console.log('Money: $' + money);
-
 
 
 
