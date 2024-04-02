c7022fe9422853624dbb64ee618828846c71aac5
Lara Kanashiro <162447628+LaraKanashiro@users.noreply.github.com>
2024-04-02 20:47:58 +0000

    cria loterrynumbers

--- exemplos-javascript/script.js
+++ exemplos-javascript/script.js
@@ -1,54 +1,31 @@
-const round = 2; //Entre 1 e 16
-let money = 1200;
-const killWeapon = 'Knife'; //'Knife', 'AWP', 'Desert Eagle', 'Molotov', 'MP7'. 
-const win = true; //false
-const howWin = 'Bomb explode'; //'Eliminando', 'Tempo esgotado', 'Bomb has been planted', 'Bomb has been defused', 'Bomb explode'.
-const defuseBomb = true; //false
-const bombDetonate = false; //true
+const lotterynumbers = []
+const firstGame = [4, 8, 15, 16, 23, 42];
 
-if (round === 1 || round === 16) {
-  money = 800;
-} else if (win === true && (howWin === 'Eliminando' || howWin === 'Tempo esgotado')) {
-  money += 3250;
-} else if (win === true && howWin === 'Bomb has been planted') {
-  money += 3550;
-} else if (win === true && (howWin === 'Bomb has been defused' || howWin === 'Bomb explode')) {
-  money += 3500;
-}
-
-if (round !== 1 && round !== 16) {
-  switch (killWeapon) {
-    case 'Knife':
-      money += 1500;
-      break;
-    case 'AWP':
-      money += 100;
-      break;
-    case 'Desert Eagle':
-      money += 300;
-      break;
-    case 'Molotov':
-      money += 300;
-      break;
-    case 'MP7':
-      money += 600;
-      break;
-    default:
-      console.log('Arma não encontrada.');
-      break;
+// Sorteio dos númenos
+for (let idex = 0; idex < 6; idex <= 1) {
+  let rondomNumber = Math.ceil(Math.random() * 60);
+  if (lotteryNumbers.includes(randomNumber)) {
+    idex - + 1;
+  } else {
+    lotteryNumbers.push(randomNumber);
   }
-}
+};
 
-if (money > 16000) {
-  money = 16000;
-}
+// Comparação de sorteio com o meu jogo
+let numberOfHits = 0;
+let hits = [];
 
-console.log('Money: $' + money);
+for (let idexLottery = 0; indexLottery < lotteryNumbers.length; indexLottery += 1) {
+  for (let indexGame = 0; indexGame < firstGame.length; indexGame += 1) {
+    if (lotteryNumbers[indexLottery] === firstGame[indexGame]) {
+      numberOfHits += 1;
+      hits.push(lotteryNumbers[indexLottery]);
+    }
+  }
+} 
 
-
-
-
-    
-
-
-
+// Quantidade de acertos 
+console.log(`Números sorteados: ${lotteryNumbers}`);
+console.log(`Jogo: ${firstGame}`);
+console.log(`Quantidade de acertos: ${numberOfHits}`);
+console.log(`Números acertados: ${hits}`);
