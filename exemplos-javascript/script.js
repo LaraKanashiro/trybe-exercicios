d5fdf3ef889caf1396210d4c1fd45cd390882282
Lara Kanashiro <162447628+LaraKanashiro@users.noreply.github.com>
2024-04-05 20:16:14 +0000

    cria a diaper

--- exemplos-javascript/script.js
+++ exemplos-javascript/script.js
@@ -1,8 +1,20 @@
-let num1 = 10;
-let num2 = 5;
+let diaper;
+let babySize = 12;
 
-console.log('Soma: ' + (num1 + num2));
-console.log('Subtração: ' + (num1 - num2));
-console.log('Multiplicação: ' + (num1 * num2));
-console.log('Divisão: ' + (num1 / num2));
-console.log('Módulo: ' + (num1 % num2));
+if(babySize <= 4){
+    diaper = 'RN';
+  } else if(babySize > 4 && babySize <= 6){
+    diaper = 'RN+';
+  } else if(babySize > 6 && babySize <= 8){
+    diaper = 'P';
+  } else if(babySize > 8 && babySize <= 10){
+    diaper = 'M';
+  } else if(babySize > 10 && babySize <= 13){
+    diaper = 'G';
+  } else if(babySize > 13 && babySize <= 15){
+    diaper = 'XG';
+  } else if(babySize > 15){
+    diaper = 'XXG';
+  }
+  
+  console.log('O tamanho da fralda é : ' + diaper);
